d9a6067ff006c7604bece0433f559a26db514876
Heather Faerber <heather@turing.com>
2023-04-17 11:32:00 -0600

    Refactor our class references

--- test/rocket-test.js
+++ test/rocket-test.js
@@ -1,34 +1,34 @@
 const expect = require('chai').expect;
 
-// const Rocket = require('../src/Rocket.js');
+const {createRocket, loadAstronauts, lightThisCandle, fillFuelTanks} = require('../rocket.js');
 
 describe('Initial setup of rocket', function() {
   it('Does not have a name', function() {
-    const rocket = new Rocket();
+    let rocket = createRocket();
 
     expect(rocket.name).to.equal('');
   });
 
   it('Has fuel tanks that are empty', function() {
-    const rocket = new Rocket();
+    let rocket = createRocket();
 
     expect(rocket.fuel).to.equal('empty');
   });
 
   it('There are no astronauts initially', function() {
-    const rocket = new Rocket();
+    let rocket = createRocket();
 
     expect(rocket.astronauts).to.deep.equal([]);
   });
 
   it('The engines are not ignited', function() {
-    const rocket = new Rocket();
+    let rocket = createRocket();
 
     expect(rocket.enginesIgnited).to.equal(false);
   });
 
   it('The cargo doors are closed and there is no cargo', function() {
-    const rocket = new Rocket();
+    let rocket = createRocket();
 
     expect(rocket.cargoDoors).to.equal('closed');
     expect(rocket.cargo).to.deep.equal([]);
@@ -38,20 +38,24 @@ describe('Initial setup of rocket', function() {
 
 describe('Functionality of rocket', function() {
   it('Should be able to load astronauts', function() {
-    const rocket = new Rocket();
+    let rocket = createRocket();
 
-    rocket.loadAstronaut('Alan Shepard');
+    rocket = loadAstronaut(rocket, 'Alan Shepard');
 
     expect(rocket.astronauts).to.deep.equal(['Alan Shepard']);
   });
 
   it('Should be able to ignite the engines', function() {
-    const rocket = new Rocket();
+    let rocket = createRocket();
 
-    rocket.lightThisCandle();
+    rocket = lightThisCandle();
 
     expect(rocket.enginesIgnited).to.equal(true);
   });
 
-  it('Should be able to fill the fuel tanks');
+  it('Should be able to fill the fuel tanks', function() {
+    //Finish this test, then write implementation code to pass all tests.  
+    //Then implement beforeEach hook to DRY up test file
+  });
+
 });
